a7fa0f05f2dbb1591613c0bea2bda4e429dd4626
BerkinChen <berkinchen@gmail.com>
2023-12-23 20:19:29 +0800

    Lv6 短路求值完成

--- src/AST/AST.cpp
+++ src/AST/AST.cpp
@@ -1,7 +1,7 @@
 #include "AST/AST.h"
 #include "AST.h"
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 // CompUnitAST
 CompUnitAST::CompUnitAST(std::unique_ptr<BaseAST> &func_def)
@@ -32,7 +32,7 @@ void *FuncDefAST::to_koopa() const {
 
   char *name = new char[ident.length() + 1];
   ("@" + ident).copy(name, ident.length() + 1);
-  name[ident.length()+1] = '\0';
+  name[ident.length() + 1] = '\0';
   ret->name = name;
 
   ret->params = slice(KOOPA_RSIK_VALUE);
@@ -233,7 +233,7 @@ void *VarDefAST::to_koopa(koopa_raw_type_t var_type) const {
   ret->ty = pointer_type_kind(KOOPA_RTT_INT32);
   char *name = new char[ident.length() + 1];
   ("@" + ident).copy(name, ident.length() + 1);
-  name[ident.length()+1] = '\0';
+  name[ident.length() + 1] = '\0';
   ret->name = name;
   ret->used_by = slice(KOOPA_RSIK_VALUE);
   ret->kind.tag = KOOPA_RVT_ALLOC;
@@ -557,17 +557,58 @@ void *LAndExpAST::make_bool(const std::unique_ptr<BaseAST> &exp) const {
 void *LAndExpAST::to_koopa() const {
   if (type == Exp)
     return eq_exp->to_koopa();
+  koopa_raw_value_data *temp = new koopa_raw_value_data();
+  temp->ty = pointer_type_kind(KOOPA_RTT_INT32);
+  temp->name = "@temp";
+  temp->used_by = slice(KOOPA_RSIK_VALUE);
+  temp->kind.tag = KOOPA_RVT_ALLOC;
+  block_manager.addInst(temp);
+  koopa_raw_value_data *temp_store = new koopa_raw_value_data();
+  temp_store->ty = type_kind(KOOPA_RTT_UNIT);
+  temp_store->name = nullptr;
+  temp_store->used_by = slice(KOOPA_RSIK_VALUE);
+  temp_store->kind.tag = KOOPA_RVT_STORE;
+  temp_store->kind.data.store.dest = temp;
+  temp_store->kind.data.store.value =
+      (koopa_raw_value_t)NumberAST(0).to_koopa();
+  block_manager.addInst(temp_store);
+  koopa_raw_value_data *branch = new koopa_raw_value_data();
+  branch->ty = type_kind(KOOPA_RTT_UNIT);
+  branch->name = nullptr;
+  branch->used_by = slice(KOOPA_RSIK_VALUE);
+  branch->kind.tag = KOOPA_RVT_BRANCH;
+  branch->kind.data.branch.cond = (koopa_raw_value_t)make_bool(and_exp);
+  koopa_raw_basic_block_data_t *true_block = new koopa_raw_basic_block_data_t();
+  true_block->name = "%true";
+  true_block->params = slice(KOOPA_RSIK_VALUE);
+  true_block->used_by = slice(KOOPA_RSIK_VALUE);
+  branch->kind.data.branch.true_bb = (koopa_raw_basic_block_t)true_block;
+  branch->kind.data.branch.true_args = slice(KOOPA_RSIK_VALUE);
+  koopa_raw_basic_block_data_t *false_block =
+      new koopa_raw_basic_block_data_t();
+  false_block->name = "%ebd";
+  false_block->params = slice(KOOPA_RSIK_VALUE);
+  false_block->used_by = slice(KOOPA_RSIK_VALUE);
+  branch->kind.data.branch.false_bb = (koopa_raw_basic_block_t)false_block;
+  branch->kind.data.branch.false_args = slice(KOOPA_RSIK_VALUE);
+  block_manager.addInst(branch);
+  block_manager.newBlock(true_block);
+  koopa_raw_value_data *true_store = new koopa_raw_value_data();
+  true_store->ty = type_kind(KOOPA_RTT_UNIT);
+  true_store->name = nullptr;
+  true_store->used_by = slice(KOOPA_RSIK_VALUE);
+  true_store->kind.tag = KOOPA_RVT_STORE;
+  true_store->kind.data.store.dest = temp;
+  true_store->kind.data.store.value = (koopa_raw_value_t)make_bool(eq_exp);
+  block_manager.addInst(true_store);
+  block_manager.addInst(jump_value(false_block));
+  block_manager.newBlock(false_block);
   koopa_raw_value_data *ret = new koopa_raw_value_data();
   ret->ty = type_kind(KOOPA_RTT_INT32);
   ret->name = nullptr;
   ret->used_by = slice(KOOPA_RSIK_VALUE);
-  ret->kind.tag = KOOPA_RVT_BINARY;
-  auto &binary = ret->kind.data.binary;
-  if (op == "&&") {
-    binary.op = KOOPA_RBO_AND;
-  }
-  binary.lhs = (koopa_raw_value_t)make_bool(and_exp);
-  binary.rhs = (koopa_raw_value_t)make_bool(eq_exp);
+  ret->kind.tag = KOOPA_RVT_LOAD;
+  ret->kind.data.load.src = temp;
   block_manager.addInst(ret);
   return ret;
 }
@@ -575,9 +616,7 @@ void *LAndExpAST::to_koopa() const {
 int LAndExpAST::cal_value() const {
   if (type == Exp)
     return eq_exp->cal_value();
-  if (op == "&&")
-    return and_exp->cal_value() && eq_exp->cal_value();
-  return 0;
+  return and_exp->cal_value() && eq_exp->cal_value();
 }
 
 // LOrExpAST
@@ -610,17 +649,58 @@ void *LOrExpAST::make_bool(const std::unique_ptr<BaseAST> &exp) const {
 void *LOrExpAST::to_koopa() const {
   if (type == Exp)
     return and_exp->to_koopa();
+  koopa_raw_value_data *temp = new koopa_raw_value_data();
+  temp->ty = pointer_type_kind(KOOPA_RTT_INT32);
+  temp->name = "@temp";
+  temp->used_by = slice(KOOPA_RSIK_VALUE);
+  temp->kind.tag = KOOPA_RVT_ALLOC;
+  block_manager.addInst(temp);
+  koopa_raw_value_data *temp_store = new koopa_raw_value_data();
+  temp_store->ty = type_kind(KOOPA_RTT_UNIT);
+  temp_store->name = nullptr;
+  temp_store->used_by = slice(KOOPA_RSIK_VALUE);
+  temp_store->kind.tag = KOOPA_RVT_STORE;
+  temp_store->kind.data.store.dest = temp;
+  temp_store->kind.data.store.value =
+      (koopa_raw_value_t)NumberAST(1).to_koopa();
+  block_manager.addInst(temp_store);
+  koopa_raw_value_data *branch = new koopa_raw_value_data();
+  branch->ty = type_kind(KOOPA_RTT_UNIT);
+  branch->name = nullptr;
+  branch->used_by = slice(KOOPA_RSIK_VALUE);
+  branch->kind.tag = KOOPA_RVT_BRANCH;
+  branch->kind.data.branch.cond = (koopa_raw_value_t)make_bool(or_exp);
+  koopa_raw_basic_block_data_t *true_block = new koopa_raw_basic_block_data_t();
+  true_block->name = "%end";
+  true_block->params = slice(KOOPA_RSIK_VALUE);
+  true_block->used_by = slice(KOOPA_RSIK_VALUE);
+  branch->kind.data.branch.true_bb = (koopa_raw_basic_block_t)true_block;
+  branch->kind.data.branch.true_args = slice(KOOPA_RSIK_VALUE);
+  koopa_raw_basic_block_data_t *false_block =
+      new koopa_raw_basic_block_data_t();
+  false_block->name = "%false";
+  false_block->params = slice(KOOPA_RSIK_VALUE);
+  false_block->used_by = slice(KOOPA_RSIK_VALUE);
+  branch->kind.data.branch.false_bb = (koopa_raw_basic_block_t)false_block;
+  branch->kind.data.branch.false_args = slice(KOOPA_RSIK_VALUE);
+  block_manager.addInst(branch);
+  block_manager.newBlock(false_block);
+  koopa_raw_value_data *false_store = new koopa_raw_value_data();
+  false_store->ty = type_kind(KOOPA_RTT_UNIT);
+  false_store->name = nullptr;
+  false_store->used_by = slice(KOOPA_RSIK_VALUE);
+  false_store->kind.tag = KOOPA_RVT_STORE;
+  false_store->kind.data.store.dest = temp;
+  false_store->kind.data.store.value = (koopa_raw_value_t)make_bool(and_exp);
+  block_manager.addInst(false_store);
+  block_manager.addInst(jump_value(true_block));
+  block_manager.newBlock(true_block);
   koopa_raw_value_data *ret = new koopa_raw_value_data();
   ret->ty = type_kind(KOOPA_RTT_INT32);
   ret->name = nullptr;
   ret->used_by = slice(KOOPA_RSIK_VALUE);
-  ret->kind.tag = KOOPA_RVT_BINARY;
-  auto &binary = ret->kind.data.binary;
-  if (op == "||") {
-    binary.op = KOOPA_RBO_OR;
-  }
-  binary.lhs = (koopa_raw_value_t)make_bool(or_exp);
-  binary.rhs = (koopa_raw_value_t)make_bool(and_exp);
+  ret->kind.tag = KOOPA_RVT_LOAD;
+  ret->kind.data.load.src = temp;
   block_manager.addInst(ret);
   return ret;
 }
@@ -628,9 +708,7 @@ void *LOrExpAST::to_koopa() const {
 int LOrExpAST::cal_value() const {
   if (type == Exp)
     return and_exp->cal_value();
-  if (op == "||")
-    return or_exp->cal_value() || and_exp->cal_value();
-  return 0;
+  return or_exp->cal_value() || and_exp->cal_value();
 }
 
 // NumberAST
